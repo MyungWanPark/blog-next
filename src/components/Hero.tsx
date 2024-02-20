e8ccf0692f8c0a8c8e468369563c9e4bdb8e1960
MyungWan Park <mywanpark@gmail.com>
2024-02-20 16:59:21 +0900

    style: hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,19 +4,22 @@ import Link from "next/link";
 
 export default function Hero() {
     return (
-        <section>
+        <section className="text-center">
             <Image
                 src={profileImage}
                 alt="profile image"
-                width={250}
-                height={250}
+                width={200}
+                height={200}
                 priority
+                className="mx-auto rounded-full aspect-square"
             />
-            <h2>I'm Myungwan</h2>
-            <h3>Full-Stack developer</h3>
+            <h2 className="text-2xl font-bold p-1">I'm Myungwan</h2>
+            <h3 className="text-xl font-semibold">Full-Stack developer</h3>
             <p>유용한 기능을 개발하는 사람</p>
             <Link href="/contact">
-                <button>Contact me</button>
+                <button className="bg-yellow-500 py-1 px-4 rounded-lg font-bold mt-2">
+                    Contact me
+                </button>
             </Link>
         </section>
     );
