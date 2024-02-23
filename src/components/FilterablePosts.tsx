06a9c81a65c81c30e99952bd4980b392bac6c0bc
MyungWan Park <mywanpark@gmail.com>
2024-02-24 01:01:49 +0900

    feat: filtered posts

---
+++ src/components/FilterablePosts.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { Post } from "@/service/posts";
+import PostsGrid from "./PostsGrid";
+import { useState } from "react";
+import Categories from "./Categories";
+
+type Props = {
+    posts: Post[];
+    categories: string[];
+};
+
+const All_Posts = "All Posts";
+
+export default function FilterablePosts({ posts, categories }: Props) {
+    const [selected, setSelected] = useState(All_Posts);
+    const filteredPosts =
+        selected === All_Posts
+            ? posts
+            : posts.filter((post) => post.category === selected);
+    console.log("filteredPosts = ", filteredPosts);
+    return (
+        <>
+            <PostsGrid posts={filteredPosts} />
+            <Categories
+                categories={[All_Posts, ...categories]}
+                onClick={setSelected}
+                selected={selected}
+            />
+        </>
+    );
+}
