9b28b54be62f91f847e6ccf9ee54b2301c96e659
MyungWan Park <mywanpark@gmail.com>
2024-02-25 01:38:53 +0900

    feat: syntax highlighter for jsx component, use next image tag in md file

--- src/components/MarkdownViewer.tsx
+++ src/components/MarkdownViewer.tsx
@@ -1,9 +1,44 @@
 import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { materialDark } from "react-syntax-highlighter/dist/esm/styles/prism";
+import Image from "next/image";
 
 export default function MarkdownViewer({ content }: { content: string }) {
     return (
-        <Markdown className="prose lg:prose-xl" remarkPlugins={[remarkGfm]}>
+        <Markdown
+            className="prose lg:prose-xl max-w-none p-2"
+            remarkPlugins={[remarkGfm]}
+            components={{
+                code(props) {
+                    const { children, className, node, ref, ...rest } = props;
+                    const match = /language-(\w+)/.exec(className || "");
+                    return match ? (
+                        <SyntaxHighlighter
+                            {...rest}
+                            style={materialDark}
+                            ref={null}
+                            PreTag="div"
+                            children={String(children).replace(/\n$/, "")}
+                            language={match[1]}
+                        />
+                    ) : (
+                        <code {...rest} className={className}>
+                            {children}
+                        </code>
+                    );
+                },
+                img: (image) => (
+                    <Image
+                        src={image.src || ""}
+                        alt={image.alt || ""}
+                        width={500}
+                        height={320}
+                        className="w-full max-h-60 object-cover"
+                    />
+                ),
+            }}
+        >
             {content}
         </Markdown>
     );
