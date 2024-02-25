c35574012627312bb16ab909b719bdfb4a1ef5c3
MyungWan Park <mywanpark@gmail.com>
2024-02-26 01:56:08 +0900

    feat: introduction of contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,40 @@
+import ContactForm from "@/components/ContactForm";
+import { AiFillGithub, AiFillLinkedin, AiFillYoutube } from "react-icons/ai";
+
+const LINKS = [
+    {
+        icon: <AiFillGithub />,
+        target: "https://github.com",
+    },
+    {
+        icon: <AiFillLinkedin />,
+        target: "https://linkedin.com/",
+    },
+    {
+        icon: <AiFillYoutube />,
+        target: "https://youtube.com",
+    },
+];
+
 export default function ContactPage() {
-    return <p>Contact me!</p>;
+    return (
+        <section className="text-center mt-4">
+            <h2 className="text-2xl font-bold">Contact me!</h2>
+            <p>info@myung.com</p>
+            <ul className="flex justify-center gap-4">
+                {LINKS.map((link, index) => (
+                    <a
+                        href={link.target}
+                        target="_blank"
+                        key={index}
+                        className="text-5xl hover:text-yellow-300"
+                    >
+                        {link.icon}
+                    </a>
+                ))}
+            </ul>
+            <h2 className="text-2xl font-semibold">Or send me an Email</h2>
+            <ContactForm />
+        </section>
+    );
 }
